bde083a079d9d3214bc669babb8f75cbaf5c0dbb
Sarvesh Khandelwal <45689268+sark-2110@users.noreply.github.com>
2020-05-13 17:03:43 +0530

    Update solution.py

--- En Route Salute/solution.py
+++ En Route Salute/solution.py
@@ -1,1 +1,21 @@
+#python3 code
 
+def count(i,s):
+    ans=0
+    for j in range(i,len(s)):
+        if(s[j]=="<"):
+            ans+=1
+    return ans
+    
+def higher(s):
+    res=0
+    for i in range(len(s)):
+        if(s[i]==">"):
+            b=count(i,s)
+            res=res+(b*2)
+    return res
+    
+def solution(s):
+    # Your code here
+    result=higher(s)
+    return result
